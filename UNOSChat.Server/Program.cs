dc4ad2704e791a6a4f006cddc5f58453f545b7e8
UNOSQUARE\ernesto.cardenas <ernesto.cardenas@unosquare.com>
2024-04-12 16:43:30 -0700

    Add project files.

---
+++ UNOSChat.Server/Program.cs
@@ -0,0 +1,42 @@
+var builder = WebApplication.CreateBuilder(args);
+
+var allowThisOrigins = "allowThisOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(allowThisOrigins, policy =>
+    {
+        policy.WithOrigins("https://localhost:4200");
+    });
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseCors(allowThisOrigins);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.MapFallbackToFile("/index.html");
+
+
+app.Run();
